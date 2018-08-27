44a1df63b76591cac04c55b8de0d22b55e5e8953
fugang <hfgayy@126.com>
2018-08-27 21:36:47 +0800

    修改测试代码

--- test/test_pin_read.py
+++ test/test_pin_read.py
@@ -1,9 +1,9 @@
 import time
 
-from xugu import Pin
+from xugu import *
 
 
-p = Pin("A2", Pin.INPUT) # 初始化2号数字信号针脚，设置为输入模式
+p = Pin(A2, Pin.INPUT) # 初始化2号数字信号针脚，设置为输入模式
 while 1:
      print(p.value())  #读取针脚电位值
      time.sleep(1)
